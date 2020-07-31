bfdd496f7757a6976147e739f4a5f1c4444ea38f
Subhamoy Karmakar <subhamoykarmakar224@gmail.com>
2020-07-31 19:22:30 +0530

    Starting EEL python from scratch

---
+++ GYMMS/GyMMS.py
@@ -0,0 +1,16 @@
+import eel
+
+
+eel.init("GUI")
+
+
+@eel.expose
+def generate_name_initials(name):
+    name = str(name).split(" ")
+    initials = ''
+    for n in name:
+        initials += n[0] + ". "
+    return initials
+
+
+eel.start('index.html', size=(1000, 600))
